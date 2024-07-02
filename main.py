49667e62e884d78a94426ee69efcdf91938ce8f8
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2024-07-02 17:49:57 +0100

    main file

--- main.py
+++ main.py
@@ -14,12 +14,30 @@
 #     recommendations = get_recommendations(user_id, n) 
 #     print(recommendations)
 
+# import os
+# from recommendations import get_recommendations
+
+# if __name__ == "__main__":
+#     user_id = int(os.getenv("USER_ID", 1))
+#     n = int(os.getenv("N", 5))
+    
+#     recommendations = get_recommendations(user_id, n)
+#     print(recommendations)
+
+
+
+import argparse
 import os
 from recommendations import get_recommendations
 
-if __name__ == "__main__":
-    user_id = int(os.getenv("USER_ID", 1))
-    n = int(os.getenv("N", 5))
-    
-    recommendations = get_recommendations(user_id, n)
+def main():
+    parser = argparse.ArgumentParser(description="Video Recommendation System")
+    parser.add_argument("user_id", type=int, help="User ID")
+    parser.add_argument("n", type=int, help="Number of recommendations")
+    args = parser.parse_args()
+
+    recommendations = get_recommendations(args.user_id, args.n)
     print(recommendations)
+
+if __name__ == "__main__":
+    main()
